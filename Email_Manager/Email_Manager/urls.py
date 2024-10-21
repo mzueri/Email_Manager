298e5857f5c3e4901378ade32cac6b6f22576205
unknown <mel.zuercher@gmail.com>
2024-10-21 11:15:34 +0200

    setup email front page

--- Email_Manager/Email_Manager/urls.py
+++ Email_Manager/Email_Manager/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/',include('email_app.urls'))
+    path('', include('email_app.urls'))
 ]
