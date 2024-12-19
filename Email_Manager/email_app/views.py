5863f0a6d8247627bf123f3f832f8e026dd4c15b
unknown <mel.zuercher@gmail.com>
2024-12-19 10:40:41 +0100

    composed emails get added to sqlite db through POST request (no JavaScript used). Inbox endpoint now displays all emails in db (needs adaptation later).

--- Email_Manager/email_app/views.py
+++ Email_Manager/email_app/views.py
@@ -2,14 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Email
 
+
 # Create your views here.
 def index(request):
     return render(request, "mail/inbox.html", {
         "emails": Email.objects.all()
     })
 
+
 def compose(request):
+    # The form is populated with submitted data only after the user submits it (via a POST request). 
+    # The first time the form is rendered (GET request), it's empty.
+    print(request.method)
+    if request.method == 'POST':
+        # Get the form data
+        sender = request.POST.get('sender')  
+        recipient = request.POST.get('recipient')
+        subject = request.POST.get('subject')
+        body = request.POST.get('body')
+
+        # Save the email to the database
+        Email.objects.create(sender=sender, recipient=recipient, subject=subject, body=body)
+        
+        return render(request, "mail/inbox.html", {"emails": Email.objects.all()})
+        
     return render(request, "mail/compose.html")
+
 
 def sent(request):
     return render(request, "mail/sent.html", {
