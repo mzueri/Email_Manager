3251b99ff44a93586f759cfc3969436659310396
unknown <mel.zuercher@gmail.com>
2025-01-10 15:54:56 +0100

    only mails for user in inbox, fix composing emails

--- Email_Manager/email_app/views.py
+++ Email_Manager/email_app/views.py
@@ -1,20 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Email
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required
 def index(request):
+    user_email = request.user.email
+    emails = Email.objects.filter(recipient=user_email)
     return render(request, "mail/inbox.html", {
-        "emails": Email.objects.all()
+        "emails": emails
     })
 
-
+@login_required
 def compose(request):
     # The form is populated with submitted data only after the user submits it (via a POST request). 
     # The first time the form is rendered (GET request), it's empty.
+    print("Before form submission:", request.session)
     print(request.method)
     if request.method == 'POST':
+        print("After form submission:", request.session)  # Log session after submitting
         # Get the form data
         sender = request.POST.get('sender')  
         recipient = request.POST.get('recipient')
@@ -24,13 +30,13 @@ def compose(request):
         # Save the email to the database
         Email.objects.create(sender=sender, recipient=recipient, subject=subject, body=body)
         
-        return render(request, "mail/inbox.html", {"emails": Email.objects.all()})
+        return redirect('inbox')
         
     return render(request, "mail/compose.html", {
         'user_email': request.user.email  # Pass the logged-in user's email
     })
 
-
+@login_required
 def sent(request):
     return render(request, "mail/sent.html", {
         "emails": Email.objects.all()
